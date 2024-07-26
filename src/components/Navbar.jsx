da022e4f33ee996593354880ec537c99b123c786
FishySenpai <akanomanbasit@gmail.com>
2024-07-26 23:23:51 +0500

    updated navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,38 +4,38 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <nav className="bg-gray-800">
+    <nav className="bg-gray-800 w-full">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="flex items-center justify-between h-16">
-          <div className="flex items-center">
-            <div className="flex-shrink-0">
+        <div className="flex items-center justify-between h-20 w-full">
+          <div className="flex flex-col 2sm:flex-row 2sm:items-center w-full">
+            <div className="flex-shrink-0 pt-4">
               <Link to="/" className="text-white font-bold">
                 Your Logo
               </Link>
             </div>
-            <div className="hidden md:block">
-              <div className="ml-10 flex items-baseline space-x-4">
+            <div className="block w-full pb-4">
+              <div className="2sm:ml-10 flex justify-end text-right space-x-4 w-full">
                 <Link
                   to="/"
-                  className="text-gray-300 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                  className="text-gray-300 hover:text-white px-3  rounded-md text-sm font-medium"
                 >
-                  Home
+                  Flights
                 </Link>
                 <Link
                   to="/hotels"
-                  className="text-gray-300 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                  className="text-gray-300 hover:text-white px-3 rounded-md text-sm font-medium"
                 >
                   Hotels
                 </Link>
                 <Link
                   to="/carhire"
-                  className="text-gray-300 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                  className="text-gray-300 hover:text-white px-3 rounded-md text-sm font-medium"
                 >
                   Car Rental
                 </Link>
                 <Link
                   to="/contact"
-                  className="text-gray-300 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                  className="text-gray-300 hover:text-white px-3 rounded-md text-sm font-medium"
                 >
                   Contact
                 </Link>
