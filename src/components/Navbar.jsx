6adc563bd35fe0fcc97745e015066c70924555b0
FishySenpai <akanomanbasit@gmail.com>
2024-07-28 23:01:09 +0500

    updated nav

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,5 @@
-// Navbar.jsx
 import React from "react";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
   return (
@@ -8,37 +7,54 @@ const Navbar = () => {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-20 w-full">
           <div className="flex flex-col 2sm:flex-row 2sm:items-center w-full">
-            <div className="flex-shrink-0 pt-4">
-              <Link to="/" className="text-white font-bold">
+            <div className="flex-shrink-0 pb-2 pt-4 2sm:pt-0">
+              <NavLink to="/" className="text-white font-bold">
                 Your Logo
-              </Link>
+              </NavLink>
             </div>
-            <div className="block w-full pb-4">
-              <div className="2sm:ml-10 flex justify-end text-right space-x-4 w-full">
-                <Link
+            <div className="block w-full pb-4 2sm:pb-0">
+              <div className="2sm:ml-10 flex justify-end text-right space-x-2 w-full">
+                <NavLink
+                  exact
                   to="/"
-                  className="text-gray-300 hover:text-white px-3  rounded-md text-sm font-medium"
+                  className={({ isActive }) =>
+                    isActive
+                      ? "text-red-600 px-3 rounded-md text-sm font-medium"
+                      : "text-gray-300 hover:text-white px-3 rounded-md text-sm font-medium"
+                  }
                 >
                   Flights
-                </Link>
-                <Link
+                </NavLink>
+                <NavLink
                   to="/hotels"
-                  className="text-gray-300 hover:text-white px-3 rounded-md text-sm font-medium"
+                  className={({ isActive }) =>
+                    isActive
+                      ? "text-red-600 px-3 rounded-md text-sm font-medium"
+                      : "text-gray-300 hover:text-white px-3 rounded-md text-sm font-medium"
+                  }
                 >
                   Hotels
-                </Link>
-                <Link
+                </NavLink>
+                <NavLink
                   to="/carhire"
-                  className="text-gray-300 hover:text-white px-3 rounded-md text-sm font-medium"
+                  className={({ isActive }) =>
+                    isActive
+                      ? "text-red-600 px-3 rounded-md text-sm font-medium"
+                      : "text-gray-300 hover:text-white px-3 rounded-md text-sm font-medium"
+                  }
                 >
                   Car Rental
-                </Link>
-                <Link
+                </NavLink>
+                <NavLink
                   to="/contact"
-                  className="text-gray-300 hover:text-white px-3 rounded-md text-sm font-medium"
+                  className={({ isActive }) =>
+                    isActive
+                      ? "text-red-600 px-3 rounded-md text-sm font-medium"
+                      : "text-gray-300 hover:text-white px-3 rounded-md text-sm font-medium"
+                  }
                 >
                   Contact
-                </Link>
+                </NavLink>
               </div>
             </div>
           </div>
